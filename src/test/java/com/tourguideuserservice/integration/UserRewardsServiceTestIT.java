258b5f862de0d4cf1b6ffb014d6458710f09540b
Loïc Stringer <loic.stringer@gmail.com>
2021-02-18 19:03:20 +0100

    Corrected test

--- src/test/java/com/tourguideuserservice/integration/UserRewardsServiceTestIT.java
+++ src/test/java/com/tourguideuserservice/integration/UserRewardsServiceTestIT.java
@@ -68,7 +68,7 @@ class UserRewardsServiceTestIT {
 		when(locationProxy.getDistancesToAttractions(userLocation.getLocation())).thenReturn(distancesToAttractionsMap);
 		when(rewardsProxy.getAttractionRewardPoints(userToReward.getUserId(), attractionAround.getAttractionId())).thenReturn(5000);
 		
-		mockMvc.perform(post("/users/"+userToReward.getUserId()+"/rewards/latest"))
+		mockMvc.perform(get("/users/"+userToReward.getUserId()+"/rewards/latest"))
 				.andExpect(status().isOk())
 				.andExpect(jsonPath("$.rewardCentralPoints").value(5000))
 				.andExpect(jsonPath("$.visitedLocationBean.userId").value(userToReward.getUserId().toString()))
