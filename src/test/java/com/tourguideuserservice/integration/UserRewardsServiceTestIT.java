fcac3143c315244516416a8fb003143e5c25ef60
Loïc Stringer <loic.stringer@gmail.com>
2021-02-18 19:04:00 +0100

    Same

--- src/test/java/com/tourguideuserservice/integration/UserRewardsServiceTestIT.java
+++ src/test/java/com/tourguideuserservice/integration/UserRewardsServiceTestIT.java
@@ -3,7 +3,6 @@ package com.tourguideuserservice.integration;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
